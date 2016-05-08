21840170a9a0f127f8a534b6d25c8076840d4c17
kazenetu <rakudafactory@gmail.com>
2016-05-08 19:56:26 +0900

    除外パスを追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var markdown = require('gulp-markdown');
 
 gulp.task('default', function () {
-    return gulp.src('./reports/**/*.md')
+    return gulp.src(['./reports/**/*.md','!./**/node_modules/**/*.md'])
         .pipe(markdown())
         .pipe(gulp.dest('./dest'));
 });
