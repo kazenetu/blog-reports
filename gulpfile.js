c8851e7bddcfc85f04cf6d3ccf1990cf576c7171
kazenetu <rakudafactory@gmail.com>
2016-05-11 21:50:34 +0900

    html変換時、htmlタグなどを出力するように修正

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,18 @@ var marked = require('marked');
 gulp.task('default', function () {
     var renderer = new marked.Renderer();
     renderer.codespan = function(code){
-      console.log(">"+code);
+      console.log("codespan>"+code);
       return '<code style="background-color: #BBB; font-weight: bold; font-size:1.2em;">' + code + '</code>';
     }
     renderer.code = function(code,lang){
-      console.log(">>"+code+":"+lang);
+      console.log("code>>"+code+":"+lang);
       return '<pre><code style="background-color: #BBB; padding-top:0.5em;padding-bottom:0.5em;">' + code + '</code></pre>';
     }
+    marked.Parser.parse = function(src, options, renderer) {
+      console.log("parser>>");
+      var parser = new marked.Parser(options, renderer);
+      return '<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body>\n'+parser.parse(src)+'</body></html>';
+    };
 
     return gulp.src(['./reports/**/*.md','!./**/node_modules/**/*.md'])
         .pipe(markdown(
