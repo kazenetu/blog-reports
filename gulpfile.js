d89ab21c4c86f7f352750661eaab219d4f7d75b0
kazenetu <rakudafactory@gmail.com>
2016-05-11 23:03:38 +0900

    コードブロックのハイライトが反映されなかった実装ミスを修正

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,16 @@ gulp.task('default', function () {
       console.log("codespan>"+code);
       return '<code style="background-color: #BBB; font-weight: bold; font-size:1.2em;">' + code + '</code>';
     }
-    renderer.code = function(code,lang){
-      console.log("code>>"+code+":"+lang);
-      return '<pre><code style="background-color: #BBB; padding-top:0.5em;padding-bottom:0.5em;">' + code + '</code></pre>';
-    }
     marked.Parser.parse = function(src, options, renderer) {
       console.log("parser>>");
       var parser = new marked.Parser(options, renderer);
-      return '<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body>\n'+parser.parse(src)+'</body></html>';
+      var parseValue = parser.parse(src)
+                        .replace(/<pre><code /g,'<pre  style="background-color: #BBB;padding:1em 1em 1em 1em;"><code ')
+                        //.replace(/<\/pre><\/code>/g,'</code>')
+                        ;
+
+
+      return '<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body>\n'+parseValue+'</body></html>';
     };
 
     return gulp.src(['./reports/**/*.md','!./**/node_modules/**/*.md'])
@@ -24,7 +26,7 @@ gulp.task('default', function () {
           {
               highlight: function (src) {
                 return hl(src).value
-                  .replace(/class="hljs-keyword"/g,'style="color: #AAA; font-weight: bold;"');
+                  .replace(/class="hljs-keyword"/g,'style="color: #00F; font-weight: bold;"');
               }
               ,renderer:renderer
           }
