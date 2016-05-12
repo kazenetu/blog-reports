5c98875669a7d37b882f6fee6b6bd8f80389ac20
kazenetu <rakudafactory@gmail.com>
2016-05-12 22:32:02 +0900

    デバッグコードを削除

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,12 @@ var marked = require('marked');
 gulp.task('default', function () {
     var renderer = new marked.Renderer();
     renderer.codespan = function(code){
-      console.log("codespan>"+code);
       return '<code style="background-color: #BBB; font-weight: bold; font-size:1.2em;">' + code + '</code>';
     }
     marked.Parser.parse = function(src, options, renderer) {
-      console.log("parser>>");
       var parser = new marked.Parser(options, renderer);
       var parseValue = parser.parse(src)
-                        .replace(/<pre><code /g,'<pre  style="background-color: #BBB;padding:1em 1em 1em 1em;"><code ')
-                        //.replace(/<\/pre><\/code>/g,'</code>')
-                        ;
-
+                        .replace(/<pre><code /g,'<pre  style="background-color: #BBB;padding:1em 1em 1em 1em;"><code ');
 
       return '<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body>\n'+parseValue+'</body></html>';
     };
